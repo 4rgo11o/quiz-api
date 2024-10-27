ce130d24709714bd4d358e51b089c1dc6ba910f3
4rgo11o <andersonged@yahoo.com.br>
2024-10-27 19:11:47 -0300

    api configurada para o ambiente de produção.

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const dotenv = require('dotenv/config.js');
 const perguntaRoutes = require('./routes/perguntas');
 const { sequelize, connectDB } = require('./config/db');
 
 const app = express();
 
+app.use(cors({ origin: 'https://magical-cranachan-3c8155.netlify.app/' }));
 app.use(express.json());
 app.use('/api/perguntas', perguntaRoutes);
 
